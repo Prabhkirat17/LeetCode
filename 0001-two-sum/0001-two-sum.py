5ce9ac114aff3d2b503167abb87261f957fb7778
Prabhkirat Kaur Arora <80431904+Prabhkirat17@users.noreply.github.com>
2025-02-10 18:26:43 -0500

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,19 @@
-class Solution(object):
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hashmap = {}
-        for i in range (len(nums)):
-            hashmap[nums[i]] = i
-        for i in range (len(nums)): 
-            x = target - nums[i]
-            if x in hashmap and hashmap[x] != i:
-                return[i, hashmap[x]]
+import java.util.HashMap;
+
+class Solution {
+  public static int[] twoSum(int[] nums, int target) {
+      // 1. Iterate over every possible number pair
+      for (int i = 0; i < nums.length; i++) {
+          // j is always ahead of i so that we don't re-evaluate already evaluated sums
+          for (int j = i + 1; j < nums.length; j++) {
+              // 2. Check if a given pair adds up to our target
+              if (nums[i] + nums[j] == target) {
+                  // Return the indices when a pair has been found
+                  return new int[]{i, j};
+              }
+          }
+      }
+      // Return an empty array if no pair is found
+      return new int[]{};
+  }
+}
